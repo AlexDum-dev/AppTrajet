6b382db93f8bdcb0fa761dc9949e979f3bf3cdc4
Le Bon Hugo <hlebon@if501-219-15.insa-lyon.fr>
2020-11-24 14:54:35 +0100

    Ajout du fichier ElemTrajet.h

---
+++ src/ElemTrajet.h
@@ -0,0 +1,80 @@
+/*************************************************************************
+                           ElemTrajet  -  description
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Interface de la classe <ElemTrajet> (fichier ElemTrajet.h) ----------------
+#if ! defined ( ELEMTRAJET_H )
+#define ELEMTRAJET_H
+
+//--------------------------------------------------- Interfaces utilisées
+#include"Trajet.h"
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <ElemTrajet>
+//
+//
+//------------------------------------------------------------------------
+
+class ElemTrajet
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    // type Méthode ( liste des paramètres );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+	
+//------------------------------------------------- Surcharge d'opérateurs
+    ElemTrajet & operator = ( const ElemTrajet & unElemTrajet );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//-------------------------------------------- Constructeurs - destructeur
+    ElemTrajet ( const ElemTrajet & unElemTrajet );
+    // Mode d'emploi (constructeur de copie) :
+    //
+    // Contrat :
+    //
+
+    ElemTrajet ( const Trajet & trajet , ElemTrajet* next ){};
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    virtual ~ElemTrajet ( ){};
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+
+Trajet* traj;
+ElemTrajet* trajSuivant;
+
+};
+
+//-------------------------------- Autres définitions dépendantes de <Xxx>
+
+#endif // XXX_H
+
