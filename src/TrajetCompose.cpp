7d40af6103b43b04fe5634f5ad024af258e2c8eb
AlexDum-dev <alexandre.dumarquez@gmail.com>
2020-12-01 19:45:27 +0100

    Implémentation d un trajet composé

---
+++ src/TrajetCompose.cpp
@@ -0,0 +1,93 @@
+/*************************************************************************
+                           Xxx  -  description
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Réalisation de la classe <TrajetCompose> (fichier TrajetCompose.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+
+//------------------------------------------------------ Include personnel
+#include "TrajetCompose.h"
+#include "ListeChainee.h"
+#include "Trajet.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+// type Xxx::Méthode ( liste des paramètres )
+// Algorithme :
+//
+//{
+//} //----- Fin de Méthode
+
+void TrajetCompose::AfficheTrajet() const 
+//Algorithme : 
+{
+	listeTrajets -> AfficheListe();
+}
+
+
+void TrajetCompose::AjouteTrajet(Trajet * unTrajet)
+// Algorithme
+{
+	listeTrajets -> AjouterTrajet(unTrajet);
+}
+
+
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+//Xxx & Xxx::operator = ( const Xxx & unXxx )
+// Algorithme :z
+//
+//{
+//} //----- Fin de operator =
+
+
+//-------------------------------------------- Constructeurs - destructeur
+//Xxx::Xxx ( const Xxx & unXxx )
+// Algorithme :
+//
+//{
+//#ifdef MAP
+//    cout << "Appel au constructeur de copie de <Xxx>" << endl;
+//#endif
+//} //----- Fin de Xxx (constructeur de copie)
+
+
+TrajetCompose::TrajetCompose(Trajet * unTrajet )
+// Algorithme :
+//
+{
+	listeTrajets = new ListeChainee(unTrajet);
+#ifdef MAP
+    cout << "Appel au constructeur de <TrajetCompose>" << endl;
+#endif
+} //----- Fin de TrajetCompose
+
+
+TrajetCompose::~TrajetCompose ( )
+// Algorithme :
+//
+{
+	delete listeTrajets;
+#ifdef MAP
+    cout << "Appel au destructeur de <TrajetCompose>" << endl;
+#endif
+} //----- Fin de ~TrajetCompose
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
+
